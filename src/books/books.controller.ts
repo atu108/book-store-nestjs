fafe8684364f6f9fc3ae9934553f47d6d195b887
iwebAtul <106381849+iwebAtul@users.noreply.github.com>
2023-08-02 11:11:16 +0530

    Get books made public

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -16,28 +16,29 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'src/prisma-client-exception.filter';
 import { Roles } from '../utils /decorators/roles.decorator';
 import { Role } from '@prisma/client';
+import { Public } from '../utils /decorators/public.decorator';
 
 const routeName = 'books';
 @Controller(routeName)
 @ApiTags(routeName)
-@ApiBearerAuth('token')
 @UseFilters(PrismaClientExceptionFilter)
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
+  @ApiBearerAuth('token')
   @Roles(Role.ADMIN)
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
-  @Roles(Role.ADMIN, Role.CUSTOMER)
+  @Public()
   @Get()
   findAll() {
     return this.booksService.findAll();
   }
 
-  @Roles(Role.ADMIN, Role.CUSTOMER)
+  @Public()
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const book = await this.booksService.findOne(+id);
